bc3420f31c60e4c2b2d58a41127b0c14b36e0a34
gibran abdillah <unkownuser145@gmail.com>
2020-10-17 22:07:07 +0700

    show image

--- gibran_app/__init__.py
+++ gibran_app/__init__.py
@@ -1,12 +1,14 @@
 from flask import (Flask,render_template,abort,request,
-                   current_app,url_for,redirect,flash)
+                   current_app,url_for,redirect,flash,send_from_directory)
 import os 
 
 
 dir_now = os.path.dirname(os.path.abspath(__file__)) # dir this file 
 app = Flask(__name__)
 
-
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg','png','jpeg','.gif'] # allowed extensions
 app.config['SECRET_KEY'] = os.urandom(12) # generate secret key
 app.config['DIR_UPLOADS'] = os.path.join(dir_now,'img') 
+
+
+list_file = os.listdir(app.config['DIR_UPLOADS'])
