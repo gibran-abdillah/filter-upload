153b0ce8d209ab4f7d5d8fd37026c0465b9a5d95
gibran abdillah <unkownuser145@gmail.com>
2020-10-17 14:31:41 +0700

    validation image

--- gibran_app/modules/controllers.py
+++ gibran_app/modules/controllers.py
@@ -1,7 +1,14 @@
 from gibran_app import *
 from gibran_app.form.validation import * 
 from werkzeug.utils import secure_filename
+import imghdr
 
+
+def validate_image(gambar):
+    idk = imghdr.what(None,gambar.read())
+    if idk != None:
+        return True 
+    return idk
 @app.route('/')
 def index_page():
     return render_template('index.html',form=FileForm())
@@ -9,14 +16,18 @@ def index_page():
 @app.route('/upload',methods=['POST','GET'])
 
 def index_upload():
-
+    form = FileForm()
     uploaded_file = request.files['my_file']
     if request.method == 'POST':
-        nama_file = uploaded_file.filename # get filename
-        ext_file = nama_file.split('.')[1] # get file extension
-        if ext_file in app.config['UPLOAD_EXTENSIONS']: # if file extension in configuration
-            uploaded_file.save(os.path.join(app.config['DIR_UPLOADS'],nama_file)) # upload your file ..
-        else:
-            abort(403) # return forbidden if file extensions is not allowed
+        if form.validate_on_submit():
+            nama_file = uploaded_file.filename # get filename
+            ext_file = nama_file.split('.')[1] # get file extension
+            
+            if ext_file in app.config['UPLOAD_EXTENSIONS'] and validate_image(uploaded_file.stream): # if file extension in configuration
+                uploaded_file.save(os.path.join(app.config['DIR_UPLOADS'],nama_file))
+                flash('File Uploaded')
+            
+            else:
+                abort(403) # return forbidden if file extensions and image are not allowed
 
     return redirect(url_for('index_page'))
