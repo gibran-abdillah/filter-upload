bc1a20b463a84f1fec111f89277889a77ed366e3
gibran abdillah <unkownuser145@gmail.com>
2020-10-17 15:29:30 +0700

    validate image

--- gibran_app/modules/controllers.py
+++ gibran_app/modules/controllers.py
@@ -5,29 +5,36 @@ import imghdr
 
 
 def validate_image(gambar):
-    idk = imghdr.what(None,gambar.read())
-    if idk != None:
+    header = gambar.read(512)
+    gambar.seek(0)
+    print(gambar.seek(0))
+    if imghdr.what(None,header) != None:
         return True 
-    return idk
+    return False
 @app.route('/')
 def index_page():
     return render_template('index.html',form=FileForm())
+
 
 @app.route('/upload',methods=['POST','GET'])
 
 def index_upload():
     form = FileForm()
     uploaded_file = request.files['my_file']
+   
     if request.method == 'POST':
         if form.validate_on_submit():
+            
             nama_file = uploaded_file.filename # get filename
             ext_file = nama_file.split('.')[1] # get file extension
-            
-            if ext_file in app.config['UPLOAD_EXTENSIONS'] and validate_image(uploaded_file.stream): # if file extension in configuration
+            if ext_file in app.config['UPLOAD_EXTENSIONS'] and validate_image(uploaded_file.stream):
                 uploaded_file.save(os.path.join(app.config['DIR_UPLOADS'],nama_file))
-                flash('File Uploaded')
+                flash('Uploaded')
+            
             
             else:
-                abort(403) # return forbidden if file extensions and image are not allowed
+               abort(403) # return forbidden if file extensions and image are not allowed
+        else:
+            flash(form.errors[0])
 
     return redirect(url_for('index_page'))
